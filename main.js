f2d9359bcd4450c4d0caac6d84e7cb57d8bd235c
David-Amir-18 <davidamir1808@gmail.com>
2025-08-17 04:07:44 +0300

    fix/search bar bug

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-// const input = document.querySelector('.search-box input');
-// input.addEventListener("keypress", (event) => {
-//     if (event.keyCode === "Enter" && (document.activeElement === input)) {
-//         event.preventDefault();
-//         window.location.href="search.html";
-//     }
-// })
-// console.log("hello world");
+const input = document.querySelector('.search-box input');
+input.addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        window.location.href="search.html";
+    }
+})
+
+console.log("hello world");
 
 // i tried something but did not work
